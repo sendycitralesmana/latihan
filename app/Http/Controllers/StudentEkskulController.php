db8cf7b97f4cba8404229f47ea591b65e5a13ec3
Lesmana <sendycitralesmana@gmail.com>
2023-07-05 18:07:56 +0700

    Sync method -many to many

--- app/Http/Controllers/StudentEkskulController.php
+++ app/Http/Controllers/StudentEkskulController.php
@@ -14,5 +14,6 @@ class StudentEkskulController extends Controller
         // $student->ekskuls()->attach([1,3]);
         $student->ekskuls()->detach(2);
         // $student->ekskuls()->attach([1,3]);
+        // $student->ekskuls()->sync([1, 3]);   -> cek data di table jika ada student:1 ekskul:1&3 maka akan dipertahankan dan sebaliknya
     }
 }
